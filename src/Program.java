7c4da019924327e3bd6352a3160d7a8c5b7eb10c
Thiago Habib <thiago.habib56@gmail.com>
2020-04-18 19:20:24 -0300

    Trying operation Map

---
+++ src/Program.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			Map<String, Integer> votes = new LinkedHashMap<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				
+				String[] fields = line.split(",");
+				String name = fields[0];
+				int count = Integer.parseInt(fields[1]);
+
+				if (votes.containsKey(name)) {
+					int votesSoFar = votes.get(name);
+					votes.put(name, count + votesSoFar);
+				}
+				else {
+					votes.put(name, count);
+				}
+				
+				line = br.readLine();
+			}
+		
+			for (String key : votes.keySet()) {
+				System.out.println(key + ": " + votes.get(key));
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
